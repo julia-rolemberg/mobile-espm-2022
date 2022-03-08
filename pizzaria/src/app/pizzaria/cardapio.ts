01c14126a544b0cb9d360e11856646f2f0e01448
julia-rolemberg <61888090+julia-rolemberg@users.noreply.github.com>
2022-03-07 21:01:23 -0300

    segunda tela: bebidas

--- pizzaria/src/app/pizzaria/cardapio.ts
+++ pizzaria/src/app/pizzaria/cardapio.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+const URL = 'https://raw.githubusercontent.com/esensato/mobile-2022-01/main/services/lista-pizza.json'
 
 @Component({
 selector: 'app-cardapio',
@@ -9,14 +12,25 @@ styleUrls: ['./cardapio.scss'],
 export class Cardapio implements OnInit {
 
     cardapio: any = [];
+    pizzaSelecionada: any;
+    bebidaSelecionada:any;
+    ocultarPizzas=false;
+
+    constructor(private http: HttpClient){}
 
     ngOnInit() {
 
-        this.cardapio = {"pizzas": [{"id": "1", "nome": "Quatro Queijos", "preco": 30.0},{"id": "2", "nome": "Frango com Catupiry", "preco": 35.0 },
-        {"id": "3", "nome": "Abobrinha", "preco": 30.0}]};
-
+        this.http.get(URL).subscribe((response) => { //subscribe permite que o código continue em execução enquanto a requesição ainda não retornou resposta
+            this.cardapio = response;
+        })
         console.log(this.cardapio)
-
+     }
+     selPizza(pizza){
+        this.pizzaSelecionada = pizza;
+        this.ocultarPizzas = true;
+     }
+     selBebida(bebida){
+         this.bebidaSelecionada = bebida;
      }
 
 }
